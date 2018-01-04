d465ec88f95ffed96bb9e1021fd25ea2901ac8c4
Evan Martin <evan.martin@gmail.com>
2018-01-04 11:21:49 -0800

    more test output

--- src/test/java/com/google/javascript/clutz/partial/provide_types_user.d.ts
+++ src/test/java/com/google/javascript/clutz/partial/provide_types_user.d.ts
@@ -1,5 +1,6 @@
 declare namespace ಠ_ಠ.clutz.module$exports$partial$provide$types$user {
-  function userFunction (klass : module$exports$partial$provide$types.Class | null , iface : module$exports$partial$provide$types.Interface | null , en : module$exports$partial$provide$types.Enum | null , nonnull : module$exports$partial$provide$types.NonNullTypedef | null , nullable : module$exports$partial$provide$types.NullableTypedef | null ) : void ;
+  function nullVariants (p : module$exports$partial$provide$types.Class_ForClosure | null , questionP : module$exports$partial$provide$types.Class_ForClosure | null , bangP : module$exports$partial$provide$types.Class_ForClosure , pOrNull : module$exports$partial$provide$types.Class_ForClosure | null ) : void ;
+  function typeVariants (klass : module$exports$partial$provide$types.Class_ForClosure | null , iface : module$exports$partial$provide$types.Interface_ForClosure | null , en : module$exports$partial$provide$types.Enum_ForClosure | null , nonnull : module$exports$partial$provide$types.NonNullTypedef_ForClosure | null , nullable : module$exports$partial$provide$types.NullableTypedef_ForClosure | null ) : void ;
 }
 declare module 'goog:partial.provide.types.user' {
   import alias = ಠ_ಠ.clutz.module$exports$partial$provide$types$user;
